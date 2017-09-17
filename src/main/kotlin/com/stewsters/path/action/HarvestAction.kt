f8edc612f9f3d568a81ede1bcd9185706754d52d
Adrian Moore <stewsters@gmail.com>
2017-09-17 14:08:22 -0500

    Some new possible actions

---
+++ src/main/kotlin/com/stewsters/path/action/HarvestAction.kt
@@ -0,0 +1,13 @@
+package com.stewsters.path.action
+
+import com.stewsters.path.entity.Entity
+
+class HarvestAction(pawn: Entity) : Action(pawn) {
+
+    override fun onPerform(): ActionResult {
+        TODO("This needs to get implemented")
+
+
+        return ActionResult.FAILURE
+    }
+}
---
+++ src/main/kotlin/com/stewsters/path/action/MountAction.kt
@@ -0,0 +1,11 @@
+package com.stewsters.path.action
+
+import com.stewsters.path.entity.Entity
+
+class MountAction(pawn: Entity) : Action(pawn) {
+
+    override fun onPerform(): ActionResult {
+        TODO("This needs to get implemented")
+        return ActionResult.FAILURE
+    }
+}

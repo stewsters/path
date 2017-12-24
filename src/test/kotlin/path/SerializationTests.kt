95da178b979011b909f70233f4f6a03e52448ca8
Adrian Moore <stewsters@gmail.com>
2017-12-24 16:29:41 -0600

    Serializing work

--- src/test/kotlin/path/SerializationTests.kt
+++ src/test/kotlin/path/SerializationTests.kt
@@ -1,26 +1,39 @@
 package path
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
 import com.stewsters.path.ecs.component.Armor
+import krogueutil.JSON
+import krogueutil.deserializeAs
+import krogueutil.serializedAs
 import org.junit.Test
 
 
 class SerializationTests {
 
     @Test
+    fun testListSerialization() {
+
+        assert(arrayOf(1, 2, 3) serializedAs JSON == "[1,2,3]")
+
+        val data = "[2,3,4]" deserializeAs Array<Int>::class.java
+
+        assert(data[0] == 2)
+        assert(data[1] == 3)
+        assert(data[2] == 4)
+    }
+
+    @Test
     fun testComponentSerialization() {
 
         val armor = Armor(5)
-        val output = jacksonObjectMapper().writeValueAsString(armor)
-
+        val output = armor serializedAs JSON
         assert(output == "{\"armor\":5}")
+
     }
 
     @Test
     fun testComponentDeserialization() {
 
-        val armor: Armor = jacksonObjectMapper().readValue("{\"armor\":5}")
+        val armor: Armor = "{\"armor\":5}" deserializeAs Armor::class.java
 
         assert(armor.armor == 5)
     }
